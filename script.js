a4c5930b86d53e8a195cbac7b037741746af47bf
Menashehata19 <161930847+Menashehata19@users.noreply.github.com>
2024-03-10 18:23:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+function share1(){
+
+    var shareData={
+
+        title:document.title,
+        text:'https://game30.netlify.app/',
+        url:document.location.href
+    }
+    if(navigator.canShare(shareData)){
+        navigator.share(shareData);
+    }
+}
+
+
+function share2(){
+
+    var shareData={
+
+        title:document.title,
+        text:'https://wallpaper7.netlify.app/',
+        url:document.location.href
+    }
+    if(navigator.canShare(shareData)){
+        navigator.share(shareData);
+    }
+}
+
+
+
+function share31(){
+
+    var shareData={
+
+        title:document.title,
+        text:' not found web',
+        url:document.location.href
+    }
+    if(navigator.canShare(shareData)){
+        navigator.share(shareData);
+    }
+}
+
+
